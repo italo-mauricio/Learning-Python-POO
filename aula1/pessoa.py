02a662d62da8b9a709dfa4d7ea0cd5f639015d26
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-09-16 15:41:29 -0300

    testando funcionalidades de POO

--- aula1/pessoa.py
+++ aula1/pessoa.py
@@ -1,16 +1,18 @@
-from mailbox import NotEmptyError
-
 
 class Pessoa:
-    def __init__(self,nome,idade):
+    def __init__(self,nome,idade, vivo=True):
         self.nome = nome
         self.idade = idade  
-              
+        self.vivo = vivo  
         
         
         
     def falar(self):
         print(f'{self.nome} esta falando...')
+        print(f'a pessoa está {self.vivo}')
+    
+    def morrer(self):
+        self.vivo = False
         
         
         
