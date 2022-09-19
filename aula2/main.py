dcdab16925fd7dd7b93ed6191226638dec106886
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-09-19 16:34:11 -0300

    update

--- aula2/main.py
+++ aula2/main.py
@@ -1,4 +1,4 @@
-
+from random import randint
 # Aula 2, método de classes
 
 class Pessoa:
@@ -17,8 +17,16 @@ class Pessoa:
     def por_ano_nascimento(cls, nome, ano_nascimento):
         idade = cls.ano_atual - ano_nascimento 
         return cls(nome, idade)
+    
+    @staticmethod
+    def gera_id():
+        rand = randint(10000, 19999)
+        return rand
+        
         
 p1 = Pessoa.por_ano_nascimento('Italo', 1998)
 print(p1)
 print(p1.nome, p1.idade)
 p1.get_ano_nascimento()
+print(Pessoa.gera_id())
+print(p1.gera_id())
