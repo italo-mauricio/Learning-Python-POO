1669b5df368a1dd2f9c2e499ced49212d6a0ebd7
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-09-22 11:15:45 -0300

    adicionando setters e getters

--- aula3.py
+++ aula3.py
@@ -9,11 +9,24 @@ class Produto:
     def desconto(self, percentual):
         self.preco = self.preco - (self.preco *(percentual / 100))
         
+    # Getter
+    
+    @property
+    def preco(self):
+        return self._preco
+    
+    # Setter
+    
+    @preco.setter
+    def preco(self, valor):
+        self._preco = valor
+        
+        
         
 p1 = Produto('camisa', 50)
 p1.desconto(10)
 print(p1.preco)
 
-p2 = Produto('sapato', 15)
+p2 = Produto('sapato', 'R$15')
 p2.desconto(10)
 print(p2.preco)
