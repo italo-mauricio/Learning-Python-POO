1f49be42307ed4121aca847e379a6745e55f70c2
italo <italogithub@gmail.com>
2022-11-21 03:22:39 -0300

    update

--- classes.py
+++ classes.py
@@ -4,7 +4,7 @@ class Meu_Objeto:
     
     def __init__(pessoa):
         pessoa.nome = 'Italo'
-        pessoa.idade = '24'
-        pessoa.tam = '1.75'
-        
+        pessoa.idade = 24
+        pessoa.tam = 1.75
+
 
